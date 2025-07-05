58601fb2b3eec17680ade725b9ac1b41fbe2e04e
Shawn <shoeless0x80@users.noreply.github.com>
2025-07-05 00:13:39 -0700

    fix tls version

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -35,7 +35,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate_validation.site_validation.certificate_arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.3_2021"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   depends_on = [aws_acm_certificate_validation.site_validation]
